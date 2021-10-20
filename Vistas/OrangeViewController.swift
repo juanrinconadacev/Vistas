006ef743c5b9104262acf56eb3fbcdc6732e67f1
Juan <juanrinconada@cev.com>
2021-10-20 12:27:06 +0200

    Add navigation examples

---
+++ Vistas/OrangeViewController.swift
@@ -0,0 +1,26 @@
+
+import UIKit
+
+class OrangeViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closeButton(_ sender: Any) {
+        dismiss(animated: false, completion: nil)
+    }
+    
+    @IBAction func goToYellowButton(_ sender: Any) {
+        if !nameTextField.text!.isEmpty {
+            goToYellow()
+        }
+    }
+    
+    func goToYellow() {
+        performSegue(withIdentifier: "goToYellow", sender: nil)
+    }
+}
