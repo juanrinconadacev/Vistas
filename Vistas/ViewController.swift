5c56750f1049a2cd6fec9c256672e0b0c521b2fc
Juan <juanrinconada@cev.com>
2021-10-26 10:37:06 +0200

    Add user defaults storage

--- Vistas/ViewController.swift
+++ Vistas/ViewController.swift
@@ -4,10 +4,35 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var saludo: UILabel!
+    
+    override func viewDidLoad() {
+        greet(name: load())
+    }
 
     @IBAction func nameChanged(_ sender: UITextField) {
         let texto = sender.text ?? "persona"
-        saludo.text = "Hola \(texto)!"
+        greet(name: texto)
+        save(name: texto)
+    }
+    
+    func save(name: String) {
+        UserDefaults.standard.set(name, forKey: "name")
+    }
+    
+    func load() -> String {
+        if let name = UserDefaults.standard.string(forKey: "name") {
+            return name
+        } else {
+            return ""
+        }
+    }
+    
+    func greet(name: String) {
+        if name.isEmpty {
+            saludo.text = "Hola!"
+        } else {
+            saludo.text = "Hola \(name)!"
+        }
     }
 }
 
